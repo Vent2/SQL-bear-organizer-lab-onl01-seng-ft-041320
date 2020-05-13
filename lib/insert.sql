b31b8caea279140a80c98aa443ddb24aa85b7ec6
Vent2 <ventura.c.gerald@gmail.com>
2020-05-13 00:14:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 16, "M", "Black", "aggresive", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 13, "F", "White", "calm", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 30, "M","Brown", "stoic", 0);
+INSERT INTO bears VALUES (5, "Melissa", 17, "F", "Red", "silly", 1);
+INSERT INTO bears VALUES (6, "Grinch", 11, "M", "Brown", "grumpy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 16, "F", "Black", "calm", 1);
+INSERT INTO bears VALUES (8, null, 57, "F", "Red", "confused", 0);
